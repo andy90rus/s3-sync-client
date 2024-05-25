c04afe62e889d3abeb3ca3b1302358de6276932a
andreykorobeynikov <andy90rus@gmail.com>
2024-05-26 01:43:39 +0300

    Updated getting bucketPrefix

--- src/commands/SyncCommand.ts
+++ src/commands/SyncCommand.ts
@@ -56,9 +56,11 @@ export class SyncCommand {
     this.options = options;
   }
 
+  private protocolRegexp = /^(s3:\/\/|http:\/\/|https:\/\/)/;
+
   async execute(client: S3Client): Promise<SyncCommandOutput> {
-    const sourceIsBucket = this.source.startsWith('s3://');
-    const targetIsBucket = this.target.startsWith('s3://');
+    const sourceIsBucket = this.protocolRegexp.test(this.source);
+    const targetIsBucket = this.protocolRegexp.test(this.target);
     if (!sourceIsBucket && !targetIsBucket) {
       throw new Error(
         'localDir to localDir sync is not supported, make sure to use s3:// prefix for buckets'
@@ -66,22 +68,38 @@ export class SyncCommand {
     }
     if (sourceIsBucket && targetIsBucket) {
       return new SyncBucketWithBucketCommand({
-        sourceBucketPrefix: this.source.substring(5),
-        targetBucketPrefix: this.target.substring(5),
+
+        sourceBucketPrefix: this.getBucketPrefix(this.source),
+        targetBucketPrefix: this.getBucketPrefix(this.target),
         ...this.options,
       } as SyncBucketWithBucketCommandInput).execute(client);
     }
     if (sourceIsBucket && !targetIsBucket) {
       return new SyncLocalWithBucketCommand({
-        bucketPrefix: this.source.substring(5),
+        bucketPrefix: this.getBucketPrefix(this.source),
         localDir: this.target,
         ...this.options,
       } as SyncLocalWithBucketCommandInput).execute(client);
     }
     return new SyncBucketWithLocalCommand({
       localDir: this.source,
-      bucketPrefix: this.target.substring(5),
+      bucketPrefix: this.getBucketPrefix(this.target),
       ...this.options,
     } as SyncBucketWithLocalCommandInput).execute(client);
+  }
+
+  private getBucketPrefix(url: string): string {
+    if (url.startsWith('s3://')) {
+      return url.substring(5);
+    }
+
+    const httpCaptureRegexp = /^[^\/]+\/\/[^\/]+\/(.*)/;
+    const match = url.match(httpCaptureRegexp);
+
+    if (match && match[1]) {
+      return match[1];
+    } else {
+      throw new Error("No match found");
+    }
   }
 }
